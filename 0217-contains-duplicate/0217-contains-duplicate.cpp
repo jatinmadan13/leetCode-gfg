1813f49d9ac067328666be1ff3318be156239147
Jatin madan <132609626+jatinmadan13@users.noreply.github.com>
2024-09-27 05:11:12 +0530

    Time: 86 ms (77.66%), Space: 73.4 MB (50.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
+        unordered_map<int,int> mpp;
+        for(int num: nums){
+            if(mpp[num]>=1){
                 return true;
             }
+            mpp[num]++;
         }
         return false;
     }
